55d32cb749cb6c74800d01ffc1eb67b6864e297b
mohit singh <mohitsingh783850@gmail.com>
2026-05-28 10:54:26 +0530

    did some changes

--- app/main.py
+++ app/main.py
@@ -13,14 +13,15 @@ app = FastAPI()
 # 1. CORS Configuration
 # =====================================================================
 origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://frontend-jobaway.vercel.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
